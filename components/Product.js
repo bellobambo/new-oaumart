ccc59f83f138a59c3268c50ade0342a45ae85375
bellobambo <bellobambo21@gmail.com>
2023-12-31 18:52:44 +0100

    toyo

--- components/Product.js
+++ components/Product.js
@@ -1,80 +1,60 @@
-
 import Link from "next/link";
 import React, { Suspense } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import 'react-lazy-load-image-component/src/effects/blur.css';
-
-
-
+import Skeleton from "./Skeleton";
 
 const Product = ({ product }) => {
-
-
   return (
-
     <>
-
-
       <div className="flex justify-center mb-20">
-        <div className=" mx-auto ">
-
-          <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box ">
+        <div className="mx-auto">
+          <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
             <div className="carousel-item min-w-[100px]">
-              <Suspense fallback={<p>Loading....</p>}>
-
-                <LazyLoadImage alt='img' placeholderSrc={product.image} effect="blur"  src={product.image} className=" max-w-[150px] h-56" />
-
+              <Suspense fallback={<Skeleton />}>
+                <LazyLoadImage
+                  alt="img"
+                  placeholderSrc={product.image}
+                  effect="blur"
+                  src={product.image}
+                  className="max-w-[150px] h-56"
+                />
               </Suspense>
-
             </div>
             <div className="carousel-item min-w-[100px]">
-              <Suspense fallback={<p>Loading....</p>}>
-
-                <LazyLoadImage alt='img' placeholderSrc={product.image} effect="blur" src={product.image2} className=" h-56 max-w-[200px]" />
+              <Suspense fallback={<Skeleton />}>
+                <LazyLoadImage
+                  alt="img"
+                  placeholderSrc={product.image}
+                  effect="blur"
+                  src={product.image2}
+                  className="h-56 max-w-[200px]"
+                />
               </Suspense>
-
             </div>
           </div>
           <div className="flex justify-between">
             <span>
               <h1>{product.name}</h1>
               <h3>₦{product.price}</h3>
-              <h3>{product.desc }</h3>
+              <h3>{product.desc}</h3>
               <h3>{product.date}</h3>
-
-            </span> 
-            
-            <span className="justify-self-end my-12" >
-
-<br></br>
+            </span>
+            <span className="justify-self-end my-12">
+              <br></br>
               <div className="badge badge-outline mx-2">{product.item}</div>
             </span>
-
           </div>
-
           <div className="text-center">
             {product.link && (
               <Link className="btn btn-active btn-accent my-3" href={product.link}>
                 Bargain
               </Link>
             )}
-
-
           </div>
         </div>
-
-
-
-
       </div>
-
-
-
     </>
-
-
-
-
   );
 };
 
