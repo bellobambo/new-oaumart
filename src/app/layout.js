a30a464d3049a9e669cab0de38574ab574837a47
bellobambo <bellobambo21@gmail.com>
2024-04-23 21:32:42 +0100

    about section

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="retro">
+    <html lang="en" data-theme="aqua">
       <body>
         <NextAuthProvider>
 
