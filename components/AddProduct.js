7ac532650972897065166b62b10e687f0393fade
bellobambo <bellobambo21@gmail.com>
2025-01-24 23:54:05 +0100

    new oau mart

--- components/AddProduct.js
+++ components/AddProduct.js
@@ -1,62 +1,60 @@
-'use client'
+"use client";
 
-
-import React from 'react';
-import Lottie from 'lottie-react';
-import Check from './Check.json'
+import React from "react";
+import Lottie from "lottie-react";
+import Check from "./Check.json";
 import { useRouter } from "next/navigation";
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { UploadButton } from '@/utils/uploadthing';
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { UploadButton } from "@/utils/uploadthing";
 
 const AddProduct = () => {
-  const [itemName, setItemName] = useState('');
-  const [brandName, setBrandName] = useState('');
-  const [itemDesc, setItemDesc] = useState('');
-  const [phone, setPhone] = useState('');
-  const [itemPrice, setItemPrice] = useState('');
-  const [image, setImage] = useState('');
+  const [itemName, setItemName] = useState("");
+  const [brandName, setBrandName] = useState("");
+  const [itemDesc, setItemDesc] = useState("");
+  const [phone, setPhone] = useState("");
+  const [itemPrice, setItemPrice] = useState("");
+  const [image, setImage] = useState("");
   const [loading, setLoading] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [productType, setProductType] = useState('');
+  const [productType, setProductType] = useState("");
 
   const router = useRouter();
 
   const { data, session } = useSession({
-    required: true
-  })
+    required: true,
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!itemName || !phone || !itemPrice || !image || !brandName) {
-      alert('Please fill in all required fields, including Product Type.');
+      alert("Please fill in all required fields, including Product Type.");
       return;
     }
 
     try {
       setLoading(true);
       const formData = new FormData();
-      formData.append('itemName', itemName);
-      formData.append('brandName', brandName);
-      formData.append('itemDesc', itemDesc);
-      formData.append('phone', phone);
-      formData.append('itemPrice', itemPrice);
-      formData.append('image', image);
+      formData.append("itemName", itemName);
+      formData.append("brandName", brandName);
+      formData.append("itemDesc", itemDesc);
+      formData.append("phone", phone);
+      formData.append("itemPrice", itemPrice);
+      formData.append("image", image);
       // formData.append('productType', productType);
 
-      const res = await fetch('https://www.oaumart.com/api/items', {
-        method: 'POST',
+      const res = await fetch("http://localhost:3001/api/items", {
+        method: "POST",
         body: formData,
       });
 
       console.log(res);
 
       if (res.ok) {
-
         setIsModalOpen(true);
 
-        console.log('Data saved successfully:', {
+        console.log("Data saved successfully:", {
           itemName,
           brandName,
           itemDesc,
@@ -66,50 +64,114 @@ const AddProduct = () => {
           // productType
         });
         setTimeout(() => {
-          setIsModalOpen(false)
-          router.push('/');
+          setIsModalOpen(false);
+          router.push("/");
         }, 2000);
       } else {
-        throw new Error('Failed to Add Item');
+        throw new Error("Failed to Add Item");
       }
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false);
-
-
     }
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen mt-[50px]">
-      <h1 className=" text-center my-4 text-[40px] font-semibold mt-4">Add Product</h1>
+      <h1 className=" text-center my-4 text-[40px] font-semibold mt-4">
+        Add Product
+      </h1>
 
       <form onSubmit={handleSubmit} className="max-w-md mx-auto text-[40px]">
         <div className="relative z-0 w-full mb-5 group">
-          <input type="text" onChange={(e) => setItemName(e.target.value)} name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer" placeholder=" " required />
-          <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-[30px]">Product Name</label>
+          <input
+            type="text"
+            onChange={(e) => setItemName(e.target.value)}
+            name="name"
+            id="name"
+            className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="name"
+            className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-[30px]"
+          >
+            Product Name
+          </label>
         </div>
 
         <div className="relative z-0 w-full mb-5 group">
-          <input type="text" onChange={(e) => setBrandName(e.target.value)} name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer" placeholder=" " required />
-          <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-[30px]">Brand Name</label>
+          <input
+            type="text"
+            onChange={(e) => setBrandName(e.target.value)}
+            name="name"
+            id="name"
+            className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="name"
+            className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-[30px]"
+          >
+            Brand Name
+          </label>
         </div>
 
         <div className="grid md:grid-cols-2 md:gap-6">
           <div className="relative z-0 w-full mb-5 group">
-            <input type="number" onChange={(e) => setItemPrice(e.target.value)} name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer" placeholder=" " required />
-            <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Product Price</label>
+            <input
+              type="number"
+              onChange={(e) => setItemPrice(e.target.value)}
+              name="floating_first_name"
+              id="floating_first_name"
+              className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="floating_first_name"
+              className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Product Price
+            </label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
-            <input onChange={(e) => setPhone(e.target.value)} type="number" name="phone" id="phone" className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer" placeholder=" " required />
-            <label htmlFor="phone" className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone Number</label>
+            <input
+              onChange={(e) => setPhone(e.target.value)}
+              type="number"
+              name="phone"
+              id="phone"
+              className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              htmlFor="phone"
+              className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Phone Number
+            </label>
           </div>
         </div>
         <div className="grid md:grid-cols-1 md:gap-6">
           <div className="relative z-0 w-full mb-5 group">
-            <input onChange={(e) => setItemDesc(e.target.value)} type="text" name="desc" id="desc" className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer" placeholder=" "/>
-            <label htmlFor="desc" className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Item Description</label>
+            <input
+              onChange={(e) => setItemDesc(e.target.value)}
+              type="text"
+              name="desc"
+              id="desc"
+              className="block py-2.5 px-0 w-full text-sm text-yellow-900 bg-transparent border-0 border-b-2 border-yellow-300 appearance-none dark:text-white dark:border-yellow-600 dark:focus:border-yellow-500 focus:outline-none focus:ring-0 focus:border-yellow-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="desc"
+              className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Item Description
+            </label>
           </div>
         </div>
         <div className="font-[12px] grid md:grid-cols-1 md:gap-6 text-[12px]">
@@ -126,7 +188,12 @@ const AddProduct = () => {
             }}
           />
         </div>
-        {image.length ? <div> <img src={image} alt='my image' width={300} height={150} /> </div> : null}
+        {image.length ? (
+          <div>
+            {" "}
+            <img src={image} alt="my image" width={300} height={150} />{" "}
+          </div>
+        ) : null}
         {/* <div className="relative text-[15px] z-0 w-full mb-5 group">
           <label classNameName="block text-white mb-2">Product Type:</label>
           <div className="flex items-center">
@@ -153,28 +220,24 @@ const AddProduct = () => {
           </div>
         </div> */}
 
-
-
-
-
-
-        <button className='btn btn-outline btn-accent' type="submit" disabled={loading}>
-          {loading ? 'Adding...' : 'Add'}
+        <button
+          className="btn btn-outline btn-accent"
+          type="submit"
+          disabled={loading}
+        >
+          {loading ? "Adding..." : "Add"}
         </button>
 
         {isModalOpen && (
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="mt-[40px] p-8 rounded-md shadow-md">
-              <div className='mb-4 z-40'>
+              <div className="mb-4 z-40">
                 <Lottie animationData={Check} />
               </div>
             </div>
           </div>
         )}
-
       </form>
-
-
     </div>
   );
 };
--- components/Product.js
+++ components/Product.js
@@ -1,18 +1,16 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import 'react-lazy-load-image-component/src/effects/blur.css';
-import Skeleton from "./Skeleton";
-import { Emailjs } from "./Emailjs";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const getItems = async () => {
   try {
-    const res = await fetch('https://www.oaumart.com/api/items', {
+    const res = await fetch("http://localhost:3001/api/items", {
       cache: "no-store",
     });
 
     if (!res.ok) {
-      throw new Error('failed to fetch items');
+      throw new Error("failed to fetch items");
     }
 
     return res.json();
@@ -20,16 +18,17 @@ const getItems = async () => {
     console.log("Error Loading Items", error);
     return [];
   }
-}
+};
 
 const Product = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [pageNumber, setPageNumber] = useState(0);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const { items } = await getItems();
       setItems(items);
       setLoading(false);
@@ -56,32 +55,23 @@ const Product = () => {
     return date.toLocaleString(); // Adjust options as needed
   };
 
-
   const displayItems = filteredItems
-  .reduce((uniqueItems, currentItem) => {
+    .reduce((uniqueItems, currentItem) => {
+      const existingItem = uniqueItems.find(
+        (item) => item.brandName === currentItem.brandName
+      );
 
-    const existingItem = uniqueItems.find(item => item.brandName === currentItem.brandName);
+      if (!existingItem) {
+        uniqueItems.push(currentItem);
+      }
 
-    if (!existingItem) {
-      uniqueItems.push(currentItem);
-    }
-
-    return uniqueItems;
-  }, [])
-  .slice()
-  .sort((a, b) => b.createdAt - a.createdAt)
-  .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-  .slice(pagesVisited, pagesVisited + itemsPerPage)
-  .map((t) => (
-    <div key={t._id} className="card w-96 bg-base-100 shadow-xl my-10">
-      {loading ? (
-        <div className="flex flex-col gap-4 w-52">
-          <div className="skeleton h-32 w-full"></div>
-          <div className="skeleton h-4 w-28"></div>
-          <div className="skeleton h-4 w-full"></div>
-          <div className="skeleton h-4 w-full"></div>
-        </div>
-      ) : (
+      return uniqueItems;
+    }, [])
+    .slice()
+    .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    .slice(pagesVisited, pagesVisited + itemsPerPage)
+    .map((t) => (
+      <div key={t._id} className="card w-96 bg-base-100 shadow-xl my-10">
         <div className=" items-center border-double border-2 border-sky-500 rounded-md p-4">
           <figure>
             <LazyLoadImage
@@ -100,21 +90,24 @@ const Product = () => {
               <div className="badge badge-outline">{t.itemPrice}</div>
             </small>
             <div className="card-actions justify-end">
-              <Link href={`https://wa.me/${t.phone}`} className='btn btn-accent'>
+              <Link
+                href={`https://wa.me/${t.phone}`}
+                className="btn btn-accent"
+              >
                 Bargain
               </Link>
             </div>
           </div>
         </div>
-      )}
-    </div>
-  ));
-
+      </div>
+    ));
 
   return (
     <>
       <div className="flex flex-col justify-center">
-        <h1 className=" text-center my-4 text-[40px] font-semibold">Our Products</h1>
+        <h1 className=" text-center my-4 text-[40px] font-semibold">
+          Our Products
+        </h1>
 
         <div className="flex justify-center text-center">
           <input
@@ -125,30 +118,35 @@ const Product = () => {
           />
         </div>
 
-        <div className="card-container sm:ml-[140px] md:ml-0 md:grid md:grid-cols-2  place-items-center">
+        {loading ? (
+          <div className="flex justify-center items-center my-8">
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+          </div>
+        ) : (
+          <>
+            <div className="card-container sm:ml-[140px] md:ml-0 md:grid md:grid-cols-2  place-items-center">
+              {displayItems}
+            </div>
 
+            <div className="flex space-x-2 justify-center text-center">
+              <button
+                className="btn btn-accent"
+                onClick={() => changePage(pageNumber - 1)}
+                disabled={pageNumber === 0}
+              >
+                Previous
+              </button>
 
-          {displayItems}
-
-        </div>
-
-        <div className="flex space-x-2 justify-center text-center">
-          <button
-            className='btn btn-accent'
-            onClick={() => changePage(pageNumber - 1)}
-            disabled={pageNumber === 0}
-          >
-            Previous
-          </button>
-      
-          <button
-            className='btn btn-accent'
-            onClick={() => changePage(pageNumber + 1)}
-            disabled={pageNumber === pageCount - 1}
-          >
-            Next
-          </button>
-        </div>
+              <button
+                className="btn btn-accent"
+                onClick={() => changePage(pageNumber + 1)}
+                disabled={pageNumber === pageCount - 1}
+              >
+                Next
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
